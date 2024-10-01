82c2cba53448baa365c2c737ed9e8eacdbb603f4
MAHAMAT263 <m.ahmat@alustudent.com>
2024-10-01 06:24:21 -0700

    Algebra

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-
 """A function that calculates the shape of a matrix,
 all elements in the same dimension are of the same
 type/shape The shape should be returned as a list of integers"""
 
+
 def matrix_shape(matrix):
-
-"""A function that calculates the shapes of a matrix"""
-
     rows = matrix
     shape = []
     while len(rows) > 0:
