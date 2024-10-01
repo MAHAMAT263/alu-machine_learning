65ed63a053224aab74bbe12e2559b8eb166a1247
MAHAMAT263 <m.ahmat@alustudent.com>
2024-10-01 05:49:26 -0700

    Algebra

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def matrix_shape(matrix):
+
+    rows = matrix
+    shape = []
+    while len(rows) > 0:
+        shape.append(len(rows))
+        rows = rows[0] if isinstance(rows[0], list) else []
+    return shape
