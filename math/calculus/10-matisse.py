35ae9770becc020844b656973574998457e60219
MAHAMAT263 <m.ahmat@alustudent.com>
2024-10-05 09:13:33 -0700

    calculus

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" derivative """
+
+
+def poly_derivative(poly):
+    """ calculates the derivative of a polynomial """
+    if not isinstance(poly, list) or not poly:
+        return None
+    if len(poly) == 1:
+        return None
+    derivative = []
+    for i in range(len(poly) -1, 0, -1)
+        derivative.append= [poly[i] * i]
+    return derivative[::1]
