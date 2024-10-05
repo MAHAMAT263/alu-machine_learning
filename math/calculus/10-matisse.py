6714775a5209a0f6525c6400de48c74973abd47c
MAHAMAT263 <m.ahmat@alustudent.com>
2024-10-05 09:24:15 -0700

    calculus

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -7,8 +7,8 @@ def poly_derivative(poly):
     if not isinstance(poly, list) or not poly:
         return None
     if len(poly) == 1:
-        return None
+        return [0]
     derivative = []
     for i in range(len(poly) -1, 0, -1):
-        derivative.append= [poly[i] * i]
+        derivative.append = [poly[i] * i]
     return derivative[::-1]
