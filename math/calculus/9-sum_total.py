0de1edf82cf0c035798975a5312085183462603f
MAHAMAT263 <m.ahmat@alustudent.com>
2024-10-05 08:37:32 -0700

    calculus

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ summation """
 
+
 def summation_i_squared(n):
     """ n is the stopping condition"""
-    if n > 0 :
+    if n > 0:
         return (n * (n + 1) * (2 * n + 1)) // 6
     else:
         return None
