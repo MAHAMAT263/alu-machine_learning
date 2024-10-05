d76677ab7688f66e017ac0d0e14dc926c7146848
MAHAMAT263 <m.ahmat@alustudent.com>
2024-10-05 09:27:54 -0700

    calculus

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 """ derivative """
 
-
 def poly_derivative(poly):
     """ calculates the derivative of a polynomial """
+    # Check if poly is a list and not empty
     if not isinstance(poly, list) or not poly:
         return None
+    
+    # Handle the case for constant polynomials
     if len(poly) == 1:
         return [0]
+    
     derivative = []
-    for i in range(len(poly) -1, 0, -1):
-        derivative.append = [poly[i] * i]
+    
+    # Calculate the derivative using the power rule
+    for i in range(len(poly) - 1, 0, -1):
+        # Append the derivative term to the list
+        derivative.append(poly[i] * i)
+
     return derivative[::-1]
