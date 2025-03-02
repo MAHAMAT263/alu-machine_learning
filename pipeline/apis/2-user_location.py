68ea1923f4b8682659eb2bdba9d066adb24d5b13
MAHAMAT263 <m.ahmat@alustudent.com>
2025-03-02 12:38:20 +0100

    Apis

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,29 +1,36 @@
 #!/usr/bin/env python3
-'''scrap from githib'''
-
+'''Scrape user location from GitHub'''
 
 import sys
 import requests
 import time
+from datetime import datetime
 
 
 def get_user_location(api_url):
-    '''return user's location'''
+    '''Return user's location'''
     try:
-        response = requests.get(api_url)
+        response = requests.get(api_url, timeout=10)  # Add timeout
         if response.status_code == 200:
             user_data = response.json()
-            print(user_data.get('location', 'Location not specified'))
+            location = user_data.get('location')
+            if location:
+                print(location)
+            else:
+                print('Location not specified')
         elif response.status_code == 404:
             print('Not found')
         elif response.status_code == 403:
             reset_time = int(response.headers.get('X-RateLimit-Reset', 0))
-            reset_in = (reset_time - time.time()) / 60
+            current_time = datetime.utcnow().timestamp()
+            reset_in = (reset_time - current_time) / 60
             print('Reset in {} min'.format(int(reset_in)))
         else:
             print('Error: {}'.format(response.status_code))
     except requests.exceptions.RequestException as e:
         print('Request failed: {}'.format(e))
+    except ValueError as e:
+        print('Invalid API URL or response: {}'.format(e))
 
 
 if __name__ == '__main__':
